9a48aeacf0d33e6dfb6b144245fc4363e1471e58
Koundjo <brendakoundjo88@gmail.com>
2024-09-30 04:12:12 +0100

    fact

--- src/main/java/com/stjean/operation/OperationMathematique.java
+++ src/main/java/com/stjean/operation/OperationMathematique.java
@@ -8,6 +8,11 @@ public class OperationMathematique {
             return false;
         }
     }
-
+	public int factoriel(int a){
+        if(a==0 || a==1){
+            return 1;
+        }
+        return a * factoriel(a-1);
+    }
 
 }
