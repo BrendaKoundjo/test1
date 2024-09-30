cbf8a9b4bbffa662c55c6a941157dbc93e68d049
Koundjo <brendakoundjo88@gmail.com>
2024-09-30 04:33:52 +0100

    trier tableau test

--- src/main/java/com/stjean/operation/OperationMathematique.java
+++ src/main/java/com/stjean/operation/OperationMathematique.java
@@ -1,18 +1,25 @@
 package com.stjean.operation;
 
+import java.util.Collections;
+import java.util.List;
+
 public class OperationMathematique {
-	public boolean estPositif(int a){
+	public static boolean estPositif(int a){
         if(a>0){
             return true;
         }else {
             return false;
         }
     }
-	public int factoriel(int a){
+	public static int factoriel(int a){
         if(a==0 || a==1){
             return 1;
         }
         return a * factoriel(a-1);
+    }
+	public static List<Integer> trier(List<Integer> listes) {
+        Collections.sort(listes, Collections.reverseOrder()); // Tri décroissant
+        return listes; // Retourner la liste triée
     }
 
 }
