dd0e9f9f1db095e973f76aeb1cc432739c4debdd
Koundjo <brendakoundjo88@gmail.com>
2024-09-30 04:09:41 +0100

    estpositif

--- src/main/java/com/stjean/operation/OperationMathematique.java
+++ src/main/java/com/stjean/operation/OperationMathematique.java
@@ -1,5 +1,13 @@
 package com.stjean.operation;
 
 public class OperationMathematique {
+	public boolean estPositif(int a){
+        if(a>0){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
 
 }
