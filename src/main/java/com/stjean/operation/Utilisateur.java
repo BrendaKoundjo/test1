e9682d341d9793d5c6aef018652e77573e88f19f
Koundjo <brendakoundjo88@gmail.com>
2024-09-30 05:10:10 +0100

    classe utilisateur ajouter

--- src/main/java/com/stjean/operation/Utilisateur.java
+++ src/main/java/com/stjean/operation/Utilisateur.java
@@ -1,5 +1,50 @@
 package com.stjean.operation;
 
 public class Utilisateur {
+    private int id;
+    private String nom;
+    private int age;
+    private String email;
+    private String telephone;
+    private String ville;
+    private double soldePersonnel;
 
+    public Utilisateur(int id, String nom, int age, String email, String telephone, String ville, double soldePersonnel) {
+        this.id = id;
+        this.nom = nom;
+        this.age = age;
+        this.email = email;
+        this.telephone = telephone;
+        this.ville = ville;
+        this.soldePersonnel = soldePersonnel;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public double getSoldePersonnel() {
+        return soldePersonnel;
+    }
 }
